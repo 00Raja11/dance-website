ab429f821eb1054335e03fb8e2e9f25eb9f4a937
Ahmad raja khan <161869332+00Raja11@users.noreply.github.com>
2025-09-27 15:12:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dance-website/", // 👈 IMPORTANT: use your repo name here
 });
